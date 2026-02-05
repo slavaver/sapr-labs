9e05d23877520ea5e1e7cc97324240516f94a79c
Vladislav Vereschagin <slavaver@gmail.com>
2026-02-05 09:52:11 +0300

    updated astro and starlite

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,38 +1,41 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
-// https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'Веб-разработка',
-			social: {
-				github: 'https://github.com/slavaver/sapr-labs',
+      title: "Веб-разработка",
+      social: [
+        {
+					icon: 'github',
+          label: "github",
+          href: "https://github.com/slavaver/sapr-labs",
 			},
+      ],
 			sidebar: [
 				{
-					label: 'Полезное',
-					autogenerate: { directory: 'useful' },
+          label: "Полезное",
+          autogenerate: { directory: "useful" },
 				},
 				{
-					label: '2025',
-					autogenerate: { directory: '2025' },
+          label: "2025",
+          autogenerate: { directory: "2025" },
 				},
 				{
-					label: '2024',
-					autogenerate: { directory: '2024' },
-					collapsed: true
+          label: "2024",
+          autogenerate: { directory: "2024" },
+          collapsed: true,
 				},
 				{
-					label: '2023',
-					autogenerate: { directory: '2023' },
-					collapsed: true
+          label: "2023",
+          autogenerate: { directory: "2023" },
+          collapsed: true,
 				},
 				{
-					label: '2022',
-					autogenerate: { directory: '2022' },
-					collapsed: true
-				}
+          label: "2022",
+          autogenerate: { directory: "2022" },
+          collapsed: true,
+        },
 			],
 		}),
 	],
