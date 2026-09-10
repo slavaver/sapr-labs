1b1331d779d235f4d078eaaaa5c4481905494f6c
Vladislav Vereschagin <slavaver@gmail.com>
2026-09-10 20:28:02 +0300

    updated starlight and astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,43 +2,49 @@ import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 
 export default defineConfig({
-  site: 'https://slavaver.github.io',
-  base: '/sapr-labs',
-  integrations: [
-    starlight({
-      title: "Веб-разработка",
-      social: [
-        {
-					icon: 'github',
-          label: "github",
-          href: "https://github.com/slavaver/sapr-labs",
-        },
-      ],
-      sidebar: [
-        {
-          label: "Полезное",
-          autogenerate: { directory: "useful" },
-        },
-        {
-          label: "2025",
-          autogenerate: { directory: "2025" },
-        },
-        {
-          label: "2024",
-          autogenerate: { directory: "2024" },
-          collapsed: true,
-        },
-        {
-          label: "2023",
-          autogenerate: { directory: "2023" },
-          collapsed: true,
-        },
-        {
-          label: "2022",
-          autogenerate: { directory: "2022" },
-          collapsed: true,
-        },
-      ],
-    }),
-  ],
+    site: "https://slavaver.github.io",
+    base: "/sapr-labs",
+    integrations: [
+        starlight({
+            title: "Веб-разработка",
+            social: [
+                {
+                    icon: "github",
+                    label: "github",
+                    href: "https://github.com/slavaver/sapr-labs",
+                },
+            ],
+            sidebar: [
+                {
+                    label: "Полезное",
+                    items: [{ autogenerate: { directory: "useful" } }],
+                },
+                {
+                    label: "2026",
+                    collapsed: true,
+                    items: [{ autogenerate: { directory: "2026" } }],
+                },
+                {
+                    label: "2025",
+                    collapsed: true,
+                    items: [{ autogenerate: { directory: "2025" } }],
+                },
+                {
+                    label: "2024",
+                    collapsed: true,
+                    items: [{ autogenerate: { directory: "2024" } }],
+                },
+                {
+                    label: "2023",
+                    collapsed: true,
+                    items: [{ autogenerate: { directory: "2023" } }],
+                },
+                {
+                    label: "2022",
+                    collapsed: true,
+                    items: [{ autogenerate: { directory: "2022" } }],
+                },
+            ],
+        }),
+    ],
 });
